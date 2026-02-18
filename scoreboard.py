69c4402156d853bf1f4362c62124edf43414c599
Jessica Kiroff <jesskiroff@gmail.com>
2026-02-18 14:13:27 -0500

    add reset function

--- scoreboard.py
+++ scoreboard.py
@@ -8,6 +8,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -16,6 +17,10 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
 
     def game_over(self):
         self.goto(0, 0)
